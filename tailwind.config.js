1c531c2718284ad7b85712906941792797a48898
JP Davy <jp.davy@icloud.com>
2022-02-05 19:01:43 -0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  mode: 'jit',
+  content: [
+    './resources/**/*.antlers.html',
+    './resources/**/*.blade.php',
+    './resources/**/*.vue',
+    './content/**/*.md'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        current: 'currentColor',
+      }
+    },
+    
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
